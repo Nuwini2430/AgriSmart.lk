c2af4fe2a41c99f53f2533441b24fea850561d0a
Nuwini <n.k.ubeysekara2430@gmail.com>
2026-03-23 02:16:21 +0530

    Remove basePath

--- frontend/agrismart/next.config.js
+++ frontend/agrismart/next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/app',
-  assetPrefix: '/app',
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true, // If you're using static export
+    unoptimized: true,
   },
 }
 
